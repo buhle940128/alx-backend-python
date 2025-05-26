62e34c1bec1199be469d9d76e562eb77143b99ee
buhle janda <jandabuhle28@gmail.com>
2025-05-26 17:41:15 +0200

    Update 0-log queries.py

--- 0-log queries.py
+++ 0-log queries.py
@@ -2,26 +2,28 @@ import functools
 import logging
 import sqlite3
 
-# Set up basic logging
+# Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def log_queries(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        query = kwargs.get("query") or (args[1] if len(args) > 1 else None)
-        if query:
-            logger.info(f"Executing SQL Query: {query}")
-        else:
-            logger.warning("No SQL query found in arguments to log.")
-        return func(*args, **kwargs)
-    return wrapper
+def log_queries():
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            query = kwargs.get("query") or (args[1] if len(args) > 1 else None)
+            if query:
+                logger.info(f"Executing SQL Query: {query}")
+            else:
+                logger.warning("No SQL query found in arguments to log.")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 class Database:
     def __init__(self, db_name):
         self.connection = sqlite3.connect(db_name)
 
-    @log_queries
+    @log_queries()
     def execute(self, query, params=None):
         cursor = self.connection.cursor()
         if params:
@@ -30,4 +32,6 @@ class Database:
             cursor.execute(query)
         self.connection.commit()
         return cursor.fetchall()
+
+
 
