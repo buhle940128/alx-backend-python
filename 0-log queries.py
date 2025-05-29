448323077859d693ce87351f1daa79e902a78856
buhle janda <jandabuhle28@gmail.com>
2025-05-29 20:04:47 +0200

    Update 0-log queries.py

--- 0-log queries.py
+++ 0-log queries.py
@@ -1,18 +1,32 @@
+import sqlite3
 import functools
 
-def log_queries():
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            print(f"[LOG] Executing function: {func.__name__}")
-            # Assuming the SQL query is passed as the first argument
-            if args:
-                query = args[0]
-                print(f"[LOG] SQL Query: {query}")
-            result = func(*args, **kwargs)
-            return result
-        return wrapper
-    return decorator
+# Decorator to log SQL queries
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Try to get the SQL query from either positional or keyword arguments
+        if args:
+            print(f"[LOG] SQL Query: {args[0]}")
+        elif 'query' in kwargs:
+            print(f"[LOG] SQL Query: {kwargs['query']}")
+        else:
+            print("[LOG] No SQL query found in arguments.")
+        
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+# Fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
 
 
 
