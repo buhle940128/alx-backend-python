b6835058ede414c73cd5cccdf42b27ba01610730
buhle janda <jandabuhle28@gmail.com>
2025-05-29 19:45:18 +0200

    Update 4-cache_query.py

--- 4-cache_query.py
+++ 4-cache_query.py
@@ -1,1 +1,16 @@
+query_cache = {}
 
+def cache_query(func):
+    """
+    Decorator to cache the result of SQL queries based on the query string.
+    """
+    def wrapper(query, *args, **kwargs):
+        if query in query_cache:
+            print("Cache hit for query:", query)
+            return query_cache[query]
+        else:
+            print("Cache miss for query:", query)
+            result = func(query, *args, **kwargs)
+            query_cache[query] = result
+            return result
+    return wrapper
