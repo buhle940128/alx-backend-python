bf4aa5b0ea35802cdd3700f785ab7be788ee5b7c
buhle janda <jandabuhle28@gmail.com>
2025-06-07 16:09:35 +0200

    Update serializers.py

--- chats/serializers.py
+++ chats/serializers.py
@@ -1,1 +1,24 @@
+from rest_framework import serializers
+from .models import CustomUser, Conversation, Message
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+      class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
 
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'conversation', 'content', 'timestamp', 'is_read']
+        read_only_fields = ['sender', 'timestamp']
+      class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'created_at', 'messages']
+     class ConversationCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants'] 
