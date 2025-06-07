92f5bea389e59b2ac2ffce19935fe51be007a4d4
buhle janda <jandabuhle28@gmail.com>
2025-06-07 15:27:28 +0200

    Update 1-execute.py

--- 1-execute.py
+++ 1-execute.py
@@ -1,1 +1,23 @@
+class ExecuteQuery:
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or ()
+        self.connection = None
+        self.cursor = None
+        self.results = None
 
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(self.query, self.params)
+        self.results = self.cursor.fetchall()
+        return self.results
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.connection:
+            if exc_type is None:
+                self.connection.commit()
+            else:
+                self.connection.rollback()
+            self.connection.close()
